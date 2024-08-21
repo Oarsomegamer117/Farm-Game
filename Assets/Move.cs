b837584ac619853d0fc33487b87b0db67190257f
Oarsomegamer117 <cooper.robinson@hibs.school.nz>
2024-08-22 10:25:58 +1200

    rocks

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Move : MonoBehaviour
@@ -9,6 +10,9 @@ public class Move : MonoBehaviour
     private float speed;
     private float timer;
     private float waitime;
+    private bool waiting;
+    private int waitnum;
+    private int walktime;
 
     void Start()
     {
@@ -16,33 +20,69 @@ public class Move : MonoBehaviour
         speed = 3;
         timer = 0;
         waitime = 0;
+        waiting = false;
+        waitnum= 0;
+
+        int num = Random.Range(1, 5);
+
+        Vector2 direction;
+
+        if (num == 1)
+            direction = Vector2.right;
+        else if (num == 2)
+            direction = Vector2.left;
+        else if (num == 3)
+            direction = Vector2.up;
+        else
+            direction = Vector2.right;
+
+        //move the character based on the above
+        rb.velocity = direction * speed;
     }
 
     void Update()
     {
         timer += Time.deltaTime;
+        waitime += Time.deltaTime;
 
-        if (timer > 1)
+        if (waiting == false)
         {
-            int num = Random.Range(1, 5);
-
-            Vector2 direction;
-
-            if (num == 1)
-                direction = Vector2.right;
-            else if (num == 2)
-                direction = Vector2.left;
-            else if (num == 3)
-                direction = Vector2.up;
-            else
-                direction = Vector2.right;
-
-            //move the character based on the above
-            rb.velocity = direction * speed;
-            timer = 0;
-          
+            if (timer > 1 + Random.Range(2, 5))
+            {
+                waiting = true;
+                timer = 0;
+            }
         }
-         if (waitime > int num = Random.Range(1, 5))
+
+        if (waiting == true)
+        {
+            rb.velocity = Vector2.zero;
+            
+         
+            if (timer > 1 + Random.Range(2, 5))
+            {
+                int num = Random.Range(1, 5);
+
+                Vector2 direction;
+
+                if (num == 1)
+                    direction = Vector2.right;
+                else if (num == 2)
+                    direction = Vector2.left;
+                else if (num == 3)
+                    direction = Vector2.up;
+                else
+                    direction = Vector2.right;
+
+                //move the character based on the above
+                timer = 0;
+                rb.velocity = direction * speed;
+                waiting = false;
+            }
+      
+            
+        }
+
 
         //pick a direction out of up, down, left, right
 
@@ -52,5 +92,9 @@ public class Move : MonoBehaviour
         //wait 2 seconds
 
         //go back to start
+
+        //Coroutine (Wait x time) does it. 
+        //Stopcouritine
     }
+
 }
