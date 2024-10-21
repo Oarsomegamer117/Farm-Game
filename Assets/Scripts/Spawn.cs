e3acfc71a0ff2fdbf85ea2d0b524742716722394
Oarsomegamer117 <cooper.robinson@hibs.school.nz>
2024-10-21 14:24:50 +1300

    Carrots Spawn

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    float SpawnTimer;
+    int xPos;
+    int yPos;
+    public GameObject Carrot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnTimer += Time.deltaTime;
+
+
+        if (SpawnTimer > 3)
+        {
+            int xPos = Random.Range(29, 42);
+            int yPos = Random.Range(-4, -26);
+
+            Vector2 pos = new Vector2(xPos, yPos);
+
+            Instantiate(Carrot, pos, Quaternion.identity);
+
+            SpawnTimer = 0;
+        }
+    }
+
+    //Random x position
+    //Random y position
+    //Make co oridinate or vector to instantiate at that position.
+    //Use timer to know when to spawn each carrot.
+    //Reset timer after each instantiation
+}
